368bf9f176377dcea55267c3b7ddf3a02b076c92
Chris Broadbent <placidbox@placidbox.com>
2017-04-24 20:06:05 +1000

    Add i3 config, clean up symlinks

--- install.sh
+++ install.sh
@@ -1,22 +1,29 @@
 #!/usr/bin/env bash
 set -eu
 
-if [ ! -h ~/.zshrc ]; then
-    # Should probably make a stub .zshrc that just `source`es the real one,
-    # rather than symlinking.
-    pushd ~
-    rm .zshrc || true
-    ln -s ~/dotfiles/zsh/zshrc.zsh .zshrc
-    popd
-fi
+# $1 -- File to remove and change to symlink
+# $2 -- dotfile to symlink in to new system
+function force_symlink {
+    if [[ $1 != "$HOME"* ]]; then # sanity check
+        echo "bad link $1 to $2"
+        exit 1
+    fi
+    if [ ! -h $1 ]; then
+        mkdir -p `dirname $1` || true
+        rm $1 || true
+        ln --no-target-directory -s $2 $1
+    fi
+}
 
-if [ ! -h ~/.config/Code/User/settings.json ]; then
-    mkdir -p ~/.config/Code/User/ || true
-    pushd ~/.config/Code/User/
-    rm settings.json keybindings.json || true
-    ln -s ~/dotfiles/code/settings.json settings.json
-    ln -s ~/dotfiles/code/keybindings.json keybindings.json
-    popd
-fi
+# ZSH config
+# Should probably make a stub .zshrc that just `source`es the real one rather than symlinking.
+force_symlink ~/.zshrc ~/dotfiles/zsh/zshrc.zsh
 
+# VS code setup, settings, keymaps, extensions
+force_symlink ~/.config/Code/User/settings.json     ~/dotfiles/code/settings.json
+force_symlink ~/.config/Code/User/keybindings.json  ~/dotfiles/code/keybindings.json
 cat code/extensions.txt | xargs -L 1 code --install-extension
+
+# i3-wm
+force_symlink ~/.config/i3/config           ~/dotfiles/i3/config
+force_symlink ~/.config/i3status/config     ~/dotfiles/i3/status_config
